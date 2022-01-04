0e7e21a081273f50425885ee0e8b0476faa25ad3
chanboklee <chanboklee@naver.com>
2022-01-05 02:22:43 +0900

    2022-01-05 initial commit

---
+++ book_shop/src/main/java/com/book/shop/domain/Member.java
@@ -0,0 +1,24 @@
+package com.book.shop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
+	private Long id;				// PK
+	private String name;			// 이름
+	private String userId;			// 유저 아이디
+	private String userPw;			// 유저 비밀번호 
+}
